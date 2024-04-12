b778580d36be1ce58bc1ccf6b86a99ba53f6e222
phoenix <phoenixwb@netc.eu>
2024-04-12 13:00:49 -0400

    fix: clean up redundant code and store entity positions on the heap for future-proofing

--- src/entities.cpp
+++ src/entities.cpp
@@ -1,11 +1,10 @@
 #include "entities.hpp"
 
-int Entity::ActiveEntities = 0;
+unsigned int Entity::ActiveEntities = 0;
 
-Entity::Entity(void) {
-  position = raylib::Vector2(0, 0);
-  id = ActiveEntities + 1;
-  ActiveEntities++;
+Entity::Entity() {
+  position = new raylib::Vector2(0, 0);
+  id = ActiveEntities++;
 }
 
-Entity::~Entity(void) { ActiveEntities--; }
+Entity::~Entity() { ActiveEntities--; }
