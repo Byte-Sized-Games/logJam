0574cc4c96f5c5ee7520633d1b87706f2da5e9af
michael <mdwmage@proton.me>
2024-04-12 10:22:57 -0400

    feat: Created Button Class
    The Button class has been created, allowing creation of basic interactable buttons that can be used to trigger events. They are very simple as of writing, and do not feature any special effects or animations, but can be expanded on in the future

--- src/ui.cpp
+++ src/ui.cpp
@@ -7,3 +7,110 @@ UiElement::UiElement(void) { position = raylib::Vector2(0, 0); }
 
 /// @brief Deconstructor. Also does nothing
 UiElement::~UiElement(void) { /* TODO */ }
+
+// -- Button Ui Element -- //
+
+/// @brief Default constructor. Leaves fields "empty"
+Button::Button(void) {}
+
+/// @brief Constructor for Button Ui element. Initialises all fields aside from box height and width
+/// @param X X coordinate for the button
+/// @param Y Y coordinate for the button
+/// @param Content Text on Button
+/// @param Background Background Colour
+/// @param Foreground Text and Outline Colour
+Button::Button(float X, float Y, std::string Content, raylib::Color Background, raylib::Color Foreground) {
+  position = raylib::Vector2(X, Y);
+  box = raylib::Rectangle(position, raylib::Vector2(80, 40));
+  content = Content;
+  background = Background;
+  foreground = Foreground;
+}
+
+/// @brief Constructor for Button Ui element. Initialises all fields aside from box height and width
+/// @param X X coordinate for the button
+/// @param Y Y coordinate for the button
+/// @param Height Box height of button
+/// @param Width Box width of button
+/// @param Content Text on Button
+/// @param Background Background Colour
+/// @param Foreground Text and Outline Colour
+Button::Button(float X, float Y, float Height, float Width, std::string Content, raylib::Color Background, raylib::Color Foreground) {
+  position = raylib::Vector2(X, Y);
+  box = raylib::Rectangle(position, raylib::Vector2(Width, Height));
+  content = Content;
+  background = Background;
+  foreground = Foreground;
+}
+
+/// @brief Deconstructor
+Button::~Button() { /* TODO */ }
+
+// -- Button Methods -- //
+
+/// @brief Checks if the button has been pressed
+/// @return Returns true if mouse down inside of button area, else return false
+bool Button::IsPressed(void) {
+  // Check that mouse left button is down
+  if (raylib::Mouse::IsButtonDown(MOUSE_BUTTON_LEFT)) {
+    // Check if mouse pointer position is within bounds of Button box.
+    if (raylib::Mouse::GetPosition().GetX() >= position.GetX() && raylib::Mouse::GetPosition().GetX() <= position.GetX() + box.GetWidth()) {
+      if (raylib::Mouse::GetPosition().GetY() >= position.GetY() && raylib::Mouse::GetPosition().GetY() <= position.GetY() + box.GetHeight()) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+/// @brief Renders Button to screen.
+/// Updates Box position, then draws Button box and Button Content to current position
+void Button::Render(void) {
+  box.SetPosition(position);
+  box.Draw(background);
+  raylib::DrawText(content.c_str(), position.GetX() + 10, position.GetY() + 10, 25, foreground);
+}
+
+// -- Button Getters -- //
+
+/// @brief Get X value of button position
+/// @return float value of buttons X position
+float Button::X(void) { return position.GetX(); }
+
+/// @brief Get Y value of button position
+/// @return float value of buttons Y position
+float Button::Y(void) { return position.GetY(); }
+
+/// @brief Get Length of button box
+/// @return float value of buttons Length
+float Button::Length(void) { return box.GetHeight(); }
+
+/// @brief Get Width of button box
+/// @return float value of buttons Width
+float Button::Width(void) { return box.GetWidth(); }
+
+/// @brief Get String Content of button
+/// @return c++ string value of buttons content
+std::string Button::Content(void) { return content; }
+
+// -- Button Setters -- //
+
+/// @brief Set X position Value of button
+/// @param X float value of new X position on Screen
+void Button::X(float X) { position.SetX(X); }
+
+/// @brief Set Y position Value of button
+/// @param Y float value of new Y position on Screen
+void Button::Y(float Y) { position.SetY(Y); }
+
+/// @brief Set Length of button box
+/// @param Length float value of new Button box Length
+void Button::Length(float Length) { box.SetHeight(Length); }
+
+/// @brief Set Width of button box
+/// @param Width float value of new Button box Width
+void Button::Width(float Width) { box.SetWidth(Width); }
+
+/// @brief Set string Content of Button
+/// @param Content c++ string value of new Button content
+void Button::Content(std::string Content) { content = Content; }
--- src/ui.hpp
+++ src/ui.hpp
@@ -1,6 +1,7 @@
 #include "inc/raylib-cpp.hpp"
 #include <string>
 
+/// @brief Base Ui Element class. All elements derive from this for polymorphism
 class UiElement {
 protected:
   raylib::Vector2 position;
@@ -8,4 +9,37 @@ protected:
 public:
   UiElement(void);
   ~UiElement(void);
+};
+
+/// @brief A button that can be used to trigger events
+class Button : UiElement {
+private:
+  std::string content;
+  raylib::Rectangle box;
+  raylib::Color background;
+  raylib::Color foreground;
+
+public:
+  // -- Constructors -- //
+  Button(void);
+  Button(float, float, std::string, raylib::Color, raylib::Color);
+  Button(float, float, float, std::string, raylib::Color, raylib::Color);
+  Button(float, float, float, float, std::string, raylib::Color, raylib::Color);
+  // -- Deconstructors -- //
+  ~Button(void);
+  // -- Methods -- //
+  bool IsPressed(void);
+  void Render(void);
+  // -- Setters -- //
+  void X(float);
+  void Y(float);
+  void Length(float);
+  void Width(float);
+  void Content(std::string);
+  // -- Getters -- //
+  float X(void);
+  float Y(void);
+  float Length(void);
+  float Width(void);
+  std::string Content(void);
 };
