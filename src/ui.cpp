b1e896631f63882a8737de9b78032557c05c62cd
michael <mdwmage@proton.me>
2024-04-12 09:54:35 -0400

    fix: comment out UiElement class
    Added some comments for the UiElement Constructor nd Deconstructor. They don't need them, but comments are comments are comments, and comments are useful

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,4 +1,9 @@
 #include "ui.hpp"
 
+// -- UiElement Skeleton Class -- //
+
+/// @brief Default constructor. Does nothing
 UiElement::UiElement(void) { position = raylib::Vector2(0, 0); }
+
+/// @brief Deconstructor. Also does nothing
 UiElement::~UiElement(void) { /* TODO */ }
