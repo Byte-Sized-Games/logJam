c125dcac59501c191b08b582d8f1b288a5ea29c1
michael <mdwmage@proton.me>
2024-04-16 12:03:00 -0400

    feat: added screen class
    Screen class makes managing menus much simpler, and reduces LOC for rendering Ui Elements. Currently broken

---
+++ src/screen.cpp
@@ -0,0 +1,40 @@
+#include "screen.hpp"
+
+// -- Screen -- //
+
+/// @brief Default constructor !DO NOT USE! it is useless and will not let you render anything meaningful
+Screen::Screen(void) { background = raylib::Color::White(); }
+
+/// @brief Secondary Constructor. Does not cover initialisation of background colour
+/// @param Menu An array of references to Ui Elements of the menu
+/// @param Length Length of the array
+Screen::Screen(std::vector<UiElement *> Menu) {
+  background = raylib::Color::White();
+  std::vector<UiElement *> menu(Menu);
+}
+
+/// @brief Primary Constructor. Does not cover initialisation of background colour
+/// @param Menu An array of references to Ui Elements of the menu
+/// @param Length Length of the array
+/// @param Background Background colour of the menu
+Screen::Screen(std::vector<UiElement *> Menu, raylib::Color Background) {
+  background = Background;
+  std::vector<UiElement *> menu(Menu);
+}
+
+/// @brief Destructor for Screen. Deletes all references and complicated objects to free memory
+Screen::~Screen(void) {
+  for (UiElement *element : menu) {
+    delete element;
+  }
+}
+
+// -- Screen Methods -- //
+
+/// @brief Renders all elements in Screen to the game
+void Screen::Render(raylib::Window *Game) {
+  Game->ClearBackground(background);
+  for (UiElement *element : menu) {
+    element->Render();
+  }
+}
---
+++ src/screen.hpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include "raylib-cpp.hpp"
+#include "ui.hpp"
+
+/// @brief Collection of Ui Elements.
+/// Used to collect and simplify elements of a menu or screen, and save lines of code when rendering
+class Screen {
+private:
+  raylib::Color background;
+  std::vector<UiElement *> menu;
+
+public:
+  Screen(void);
+  ~Screen(void);
+  Screen(std::vector<UiElement *>);
+  Screen(std::vector<UiElement *>, raylib::Color);
+  void Render(raylib::Window *);
+};
