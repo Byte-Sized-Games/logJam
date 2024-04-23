237644fe244c6ae11e2d790804f66c3a4dac70aa
michael <mdwmage@proton.me>
2024-04-23 13:04:29 -0400

    feat: using entity namespace properly.
    just what the title says.

--- src/entities.cpp
+++ src/entities.cpp
@@ -1,10 +1,10 @@
 #include "entities.hpp"
 
-unsigned int Entity::ActiveEntities = 0; // Set Initial Amount of active entities (gameObjects)
+unsigned int entity::Entity::ActiveEntities = 0; // Set Initial Amount of active entities (gameObjects)
 
-Entity::Entity() {
-  position = new raylib::Vector2(0, 0);
-  id = ActiveEntities++;
+entity::Entity::Entity() {
+	position = new raylib::Vector2(0, 0);
+	id = ActiveEntities++;
 }
 
-Entity::~Entity() { ActiveEntities--; }
+entity::Entity::~Entity() { ActiveEntities--; }
--- src/entities.hpp
+++ src/entities.hpp
@@ -1,12 +1,29 @@
 #include "raylib-cpp.hpp"
 
+namespace entity {
+enum direction { north, east, south, west };
 class Entity {
-protected:
-  raylib::Vector2 *position;
-  int id;
+  protected:
+	raylib::Vector2 *position;
+	int id;
 
-public:
-  static unsigned int ActiveEntities;
-  Entity(void);
-  ~Entity(void);
+  public:
+	static unsigned int ActiveEntities;
+	Entity(void);
+	~Entity(void);
 };
+
+class Player : public Entity {
+  private:
+	raylib::Texture2D *spriteSheet;
+	raylib::Texture2D *currentSprite;
+	unsigned int speed;
+
+  public:
+	Player();
+	Player(float, float);
+	~Player();
+
+	void Move(direction);
+};
+} // namespace entity
