6d6a38b8de3c8c30ac1b67f34f46c20fd73d4344
michael <mdwmage@proton.me>
2024-05-02 18:17:16 -0400

    fix: button test
    quick 'n dirty implementation of a button on the homescreen, and a little test to check its interactivity

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,29 @@
 // created by Michael Ward on 01/05/24
 //
 
+// Declare modules
+mod entities;
+mod ui;
+// ---
+
 use macroquad::prelude::*;
+use ui::Ui;
 
 #[macroquad::main("logjam")]
 async fn main() {
+    let button = ui::button::Button::new(50.0, 100.0, "Play".to_string(), DARKBLUE, WHITE);
     loop {
         clear_background(DARKGRAY);
 
         draw_text("IT WORKS!", 20.0, 40.0, 30.0, WHITE);
+        button.render();
+
+        // Get button clicked
+        if button.interacted() {
+            println!("Pressed!");
+        } else {
+            println!("Not pressed!");
+        }
 
         next_frame().await
     }
