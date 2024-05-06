bc78c3591b22eab48a5eee95a70ee97ef28425a1
michael <mdwmage@proton.me>
2024-05-06 15:50:19 -0400

    fix: improve window config
    Window config is met in a much nicer way. Other synactic changes included too

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,28 @@ mod screen;
 mod ui;
 // ---
 
+// Namespaces
 use macroquad::prelude::*;
+use screen::*;
 use ui::*;
+// ---
 
-#[macroquad::main("logjam")]
+// Constants
+const WINDOW_HEIGHT: u32 = 800;
+const WINDOW_WIDTH: u32 = 800;
+// ---
+
+#[macroquad::main(window_conf)]
 async fn main() {
     let button = button::Button::new(50.0, 100.0, "Play".to_string(), DARKBLUE, WHITE);
     let title = text_object::TextObject::new(30.0, 40.0, "logger".to_string(), 45.0, WHITE);
+    let title_screen = Menu {
+        elements: vec![Box::new(&button), Box::new(&title)],
+    };
     loop {
         clear_background(SKYBLUE);
 
-        title.render();
-        button.render();
+        title_screen.render();
 
         // Get button clicked
         if button.interacted() {
@@ -29,5 +39,15 @@ async fn main() {
         }
 
         next_frame().await
+    }
+}
+
+/// Function to initialise game window
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "logger".to_owned(),
+        window_width: WINDOW_WIDTH as i32,
+        window_height: WINDOW_HEIGHT as i32,
+        ..Default::default()
     }
 }
