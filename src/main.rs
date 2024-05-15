c1d4bc37e645621bc6dc0cafe59b72fc5675094f
phoenix <phoenixwb@netc.eu>
2024-05-15 10:21:43 -0400

    fully unbreak

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,13 @@ struct GameState {
     /// @brief scene-specific draw callstack. runs after master callstack on each priority level
     draw_stack: Vec<Vec<bool>>,
     /// @brief vector of elements drawn on first state call via a draw_stack item
-    init_scene: Vec<*const Entity>
+    init_scene: Vec<*const dyn Ui>
 }
 
 
 mod runtime {
+    use crate::GameState;
+
 
     /// @brief master logic callstack. not recommended in most cases, try pushing to a scene-specific callstack to avoid transition hell
     static mut LOGIC_STACK : Vec<Vec<bool>> = vec![];
@@ -33,14 +35,18 @@ mod runtime {
 
     /// @brief currently rendering GameState
     // TODO: implement init scene
-    static mut CURRENT_STATE : GameState = 0;
+    static mut CURRENT_STATE : GameState = GameState{
+        logic_stack: vec![],
+        draw_stack: vec![],
+        init_scene: vec![],
+    };
 
 }
 fn load_state() {
 
 }
 
-#[macroquad::main("logjam")]
+// #[macroquad::main("logjam")]
 
 // Constants
 const WINDOW_HEIGHT: u16 = 800;
