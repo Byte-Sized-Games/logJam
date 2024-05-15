a3f1491239b602c85d47f38bab2feb8bd9c63aaf
michael <mdwmage@proton.me>
2024-05-15 10:46:44 -0400

    fix: remove unneeded comment for macroquad macro
    That just doesn't really need to be there

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,7 @@ const WINDOW_WIDTH: u16 = 800;
 // ---
 
 #[macroquad::main(window_conf)]
+
 async fn main() {
     let button = button::Button::new(50.0, 100.0, "Play".to_string(), DARKBLUE, WHITE);
     let title = text_object::TextObject::new(30.0, 40.0, "logger".to_string(), 45.0, WHITE);
