0c49987b8a688f4d9d07cfcff9beaa0db1cef05c
phoenix <phoenixwb@netc.eu>
2024-05-15 10:08:23 -0400

    rust is my favorite guy(feat: scaffold game states and runtime variables)

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,41 @@ use screen::*;
 use ui::*;
 // ---
 
+struct GameState {
+    /// @brief scene-specific logic callstack. runs after master callstack on each priority level
+    logic_stack: Vec<Vec<bool>>,
+    /// @brief scene-specific draw callstack. runs after master callstack on each priority level
+    draw_stack: Vec<Vec<bool>>,
+    /// @brief vector of elements drawn on first state call via a draw_stack item
+    init_scene: Vec<*Entity>
+}
+
+
+mod runtime {
+
+    /// @brief master logic callstack. not recommended in most cases, try pushing to a scene-specific callstack to avoid transition hell
+    static mut LOGIC_STACK : Vec<Vec<bool>> = vec![];
+    /// @brief master draw callstack. not recommended in most cases, try pushing to a scene-specific callstack to avoid transition hell
+    static mut DRAW_STACK : Vec<Vec<bool>> = vec![];
+
+    /// @brief currently rendering GameState
+    // TODO: implement init scene
+    static mut CURRENT_STATE : GameState = 0;
+
+}
+fn load_state() {
+
+}
+
+#[macroquad::main("logjam")]
+
 // Constants
 const WINDOW_HEIGHT: u32 = 800;
 const WINDOW_WIDTH: u32 = 800;
 // ---
 
 #[macroquad::main(window_conf)]
+
 async fn main() {
     let button = button::Button::new(50.0, 100.0, "Play".to_string(), DARKBLUE, WHITE);
     let title = text_object::TextObject::new(30.0, 40.0, "logger".to_string(), 45.0, WHITE);
