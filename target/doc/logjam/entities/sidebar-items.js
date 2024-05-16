9da14188ef66396f535f4fcf3e090f70c56e0514
michael <mdwmage@proton.me>
2024-05-16 14:17:46 -0400

    feat: documentation action
    We documenting the project with this one

--- target/doc/logjam/entities/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["Player"],"trait":["Entity"]};
--- target/doc/src-files.js
+++
@@ -1,4 +0,0 @@
-var srcIndex = new Map(JSON.parse('[\
-["logjam",["",[["entities",[],["mod.rs"]],["screen",[],["mod.rs"]],["ui",[],["button.rs","mod.rs","text_object.rs"]]],["main.rs"]]]\
-]'));
-createSrcSidebar();
