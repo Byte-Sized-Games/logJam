e114ecda9efc3d9376ec3ab25611f08f4e1b44d5
michael <mdwmage@proton.me>
2024-05-21 14:34:13 -0400

    feat: Rendering the MAP
    the cubes represent all.

--- src/screen/map.rs
+++ src/screen/map.rs
@@ -1,8 +1,7 @@
 use crate::{entities::Tile, Call};
 use macroquad::{
-    color::{BEIGE, BROWN, SKYBLUE},
-    shapes::{draw_rectangle, draw_rectangle_lines},
-    window::{screen_height, screen_width},
+    color::{BROWN, SKYBLUE},
+    shapes::draw_rectangle,
 };
 use serde::Deserialize;
 use std::fs;
@@ -11,24 +10,13 @@ use std::fs;
 pub struct Map {
     pub name: String,
     pub hi_score: u32,
-    pub width: u32,
-    pub height: u32,
     pub tiles: Vec<Vec<Tile>>,
 }
 
 impl Map {
-    pub fn new(name: &str, hi_score: u32, width: u32, height: u32) -> Self {
-        Map {
-            name: name.to_owned(),
-            hi_score,
-            width,
-            height,
-            tiles: vec![vec![]],
-        }
-    }
     pub fn render(&self) {
-        let mut x = screen_width() - self.width as f32;
-        let mut y = screen_height() - self.height as f32;
+        let mut x = 0.0;
+        let mut y = 0.0;
 
         for column in &self.tiles {
             for tile in column {
@@ -40,7 +28,7 @@ impl Map {
                 y += 100.0;
             }
             x += 100.0;
-            y = screen_height() - self.height as f32;
+            y = 0.0;
         }
     }
 }
@@ -54,4 +42,11 @@ impl Call for Map {
 pub fn parse_config(path: &str) -> Map {
     let config = fs::read_to_string(path).expect("Couldn't read config");
     toml::from_str(&config).unwrap()
+}
+
+pub fn check_tile(tile: &Tile) {
+    match tile {
+        Tile::Log => (),
+        Tile::Water => println!("wet :("),
+    }
 }
