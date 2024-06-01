cb4d68df99cf9955b31544376d51e01f5bfff388
brandiob <brandiob@users.noreply.github.com>
2024-06-01 12:58:28 -0400

    file structure changes

--- cpp-things/main.cpp
+++ cpp-things/main.cpp
@@ -12,7 +12,7 @@ int main() {
     std::string player = "Player1";
     int time = 120;
 
-    //ooo first time with new header system
+    // first time with new header system
     DatabaseManager::insertData(dir, level, score, player, time);
 
     return 0;
